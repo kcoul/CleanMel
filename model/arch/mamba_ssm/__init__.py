6dde8ec16d98acdb6a1008eede30084b2653f5d8
Kieran Coulter <kicoulter@qnx.com>
2026-02-23 18:06:23 -0800

    Fix import paths

--- model/arch/mamba_ssm/__init__.py
+++ model/arch/mamba_ssm/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "1.1.1"
 
-from ops.selective_scan_interface import selective_scan
-from modules.mamba_simple import Mamba
-from models.mixer_seq_simple import MambaLMHeadModel
+from .ops.selective_scan_interface import selective_scan
+from .modules.mamba_simple import Mamba
+from .models.mixer_seq_simple import MambaLMHeadModel
